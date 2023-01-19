149b5e6b7e2691f3f21f0596e65bd617df108886
xmic <xmic@fi.muni.cz>
2023-01-19 13:56:54 +0100

    -init

---
+++ src/main/java/vm/fs/FSGlobal.java
@@ -0,0 +1,15 @@
+package vm.fs;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author xmic
+ */
+public class FSGlobal {
+
+    private static final Logger LOG = Logger.getLogger(FSGlobal.class.getName());
+    
+    
+
+}
