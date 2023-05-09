57214e30a2ea9f72d40ac8c2ec6b0fa7becc0585
xmic <xmic@fi.muni.cz>
2023-05-09 19:20:43 +0200

    minor

--- src/main/java/vm/fs/main/groundTruth/FSEvaluateGroundTruthMain.java
+++ src/main/java/vm/fs/main/groundTruth/FSEvaluateGroundTruthMain.java
@@ -19,9 +19,13 @@ public class FSEvaluateGroundTruthMain {
     public static void main(String[] args) {
         Dataset[] datasets = new Dataset[]{
             //            new M2DatasetInstanceSingularizator.DeCAF20MDataset()
-            new FSDatasetInstanceSingularizator.LAION_100k_Dataset()
+            new FSDatasetInstanceSingularizator.LAION_300k_Dataset(),
+            new FSDatasetInstanceSingularizator.LAION_10M_Dataset(),
+            new FSDatasetInstanceSingularizator.LAION_30M_Dataset(),
+            new FSDatasetInstanceSingularizator.LAION_100M_Dataset()
         };
         for (Dataset dataset : datasets) {
+            System.gc();
             String datasetName = dataset.getDatasetName();
             String querySetName = dataset.getQuerySettName();
             int k = 1000; // 1000 for orig datasets, else 50000
