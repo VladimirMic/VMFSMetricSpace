19fcd5181f9fce05230f7bd8fc9035f698e19987
VladimirMic <xmic@fi.muni.cz>
2024-05-20 15:30:38 +0200

    stop state - producing big filtering coefs for data dep metric filtering

--- src/main/java/vm/fs/main/search/filtering/learning/FSLearnCoefsForDataDepenentMetricFilteringMain.java
+++ src/main/java/vm/fs/main/search/filtering/learning/FSLearnCoefsForDataDepenentMetricFilteringMain.java
@@ -1,14 +1,16 @@
 package vm.fs.main.search.filtering.learning;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import vm.fs.dataset.FSDatasetInstanceSingularizator;
 import vm.fs.main.precomputeDistances.FSEvalAndStoreObjectsToPivotsDistsMain;
 import vm.fs.store.auxiliaryForDistBounding.FSTriangleInequalityWithLimitedAnglesCoefsStorageImpl;
-import vm.fs.store.precomputedDists.FSPrecomputedDistPairsStorageImpl;
+import vm.metricSpace.AbstractMetricSpace;
 import vm.metricSpace.Dataset;
+import vm.metricSpace.distance.DistanceFunctionInterface;
 import vm.metricSpace.distance.bounding.onepivot.learning.LearningTriangleInequalityWithLimitedAngles;
 import static vm.metricSpace.distance.bounding.onepivot.learning.LearningTriangleInequalityWithLimitedAngles.RATIO_OF_SMALLEST_DISTS;
-import vm.metricSpace.distance.storedPrecomputedDistances.PrecomputedPairsOfDistancesStoreInterface;
 
 /**
  *
@@ -57,11 +59,16 @@ public class FSLearnCoefsForDataDepenentMetricFilteringMain {
     }
 
     public static void run(Dataset dataset) {
+        AbstractMetricSpace metricSpace = dataset.getMetricSpace();
+        DistanceFunctionInterface df = dataset.getDistanceFunction();
         int pivotCount = FSEvalAndStoreObjectsToPivotsDistsMain.PIVOT_COUNT;
-        PrecomputedPairsOfDistancesStoreInterface smallDistSample = new FSPrecomputedDistPairsStorageImpl(dataset.getDatasetName(), SAMPLE_O_COUNT, SAMPLE_Q_COUNT);
+        List sampleOfDataset = dataset.getSampleOfDataset(SAMPLE_O_COUNT + SAMPLE_Q_COUNT);
+        List sampleOfQueries = new ArrayList(sampleOfDataset.subList(0, SAMPLE_Q_COUNT));
+        sampleOfDataset.removeAll(sampleOfQueries);
+        List pivots = dataset.getPivots(pivotCount);
 
         FSTriangleInequalityWithLimitedAnglesCoefsStorageImpl storage = new FSTriangleInequalityWithLimitedAnglesCoefsStorageImpl();
-        LearningTriangleInequalityWithLimitedAngles learning = new LearningTriangleInequalityWithLimitedAngles(dataset, smallDistSample, pivotCount, SAMPLE_O_COUNT, SAMPLE_Q_COUNT, storage, dataset.getDatasetName());
+        LearningTriangleInequalityWithLimitedAngles learning = new LearningTriangleInequalityWithLimitedAngles(metricSpace, df, pivots, sampleOfDataset, sampleOfQueries, storage, dataset.getDatasetName());
         learning.execute();
     }
 
