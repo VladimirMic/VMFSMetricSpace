119f1c5e17b84d4f14383d84f238d84ace9a5739
xmic <xmic@fi.muni.cz>
2025-06-10 20:13:30 +0200

    new dataset

--- src/main/java/vm/fs/main/datatools/FSPrepareNewDatasetForPivotFilterings.java
+++ src/main/java/vm/fs/main/datatools/FSPrepareNewDatasetForPivotFilterings.java
@@ -46,6 +46,7 @@ public class FSPrepareNewDatasetForPivotFilterings {
 //            FSLayersKasperStorage.createDataset(FSLayersKasperStorage.TYPE_0_small, FSLayersKasperStorage.DIMENSION_0_small)
 //            new FSDatasetInstances.DeCAF20M_PCA256Dataset(),
             new FSDatasetInstances.DeCAFDataset(),
+//            new FSDatasetInstances.Yahoo100M_1MSubset_Dataset(),
 //            new FSDatasetInstanceSingularizator.DeCAF100M_Dataset(),
 //            new FSDatasetInstanceSingularizator.DeCAF100M_PCA256Dataset(),
         //            new FSDatasetInstanceSingularizator.Faiss_Clip_100M_PCA256_Candidates(),
